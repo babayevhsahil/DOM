7d1c57ef73994a67c6d381713f152864aac973ca
sahilbabayev44 <107620978+sahilbabayev44@users.noreply.github.com>
2022-09-14 05:54:28 +0400

    Saat 6-ya kimi Etdiyim Task

---
+++ Js/app.js
@@ -0,0 +1,27 @@
+
+
+
+
+var btn_add = document.getElementById('btn_add');
+
+
+
+btn_add.onclick = function() {
+  var value = document.getElementById('input').value;
+  var h3 = document.createElement('h3');
+  var icon = document.createElement('i');
+  var div = document.createElement('div');
+  div.setAttribute('class', 'box');
+  icon.setAttribute('class','fa-solid fa-trash-can');
+  h3.innerHTML = value;
+  //h3.setAttribute('class','p228')
+  var todo = document.getElementById('todos');
+  div.appendChild(h3);
+  div.appendChild(icon);
+  todo.appendChild(div);
+  icon.addEventListener("click", (e) => {
+    var parent = e.target.parentElement;
+    parent.remove();
+})
+
+}
